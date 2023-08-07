1ee7180ba39519a3e054e08ccb568b0609a5bb65
igrok1803444 <v618591@gmail.com>
2023-08-07 12:40:55 +0300

    end first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//пошук елементів
+const galleryList = document.querySelector('.gallery');
+//слухачі подій
+galleryList.addEventListener('click', showLargeImage);
+//розмітка галереї 
+const galleryTemplate = galleryItems.map(({ description, original, preview }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}).join('');
+ 
+galleryList.insertAdjacentHTML('beforeend', galleryTemplate);
 
-console.log(galleryItems);
+//функція для слухача подій 
+function showLargeImage(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}"/>
+    </div>
+`)
+    instance.show();
+    galleryList.addEventListener('keydown', event => {
+        if (event.code === 'Escape') {
+            instance.close()
+            galleryList.removeEventListener('keydown', showLargeImage)
+        }
+    })
+}
+
+
+
